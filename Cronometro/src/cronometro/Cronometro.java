e470ae7f8bd1ce6a9376059b3a47cf152d46f645
andrelucassvt <andrelucassvt99@gmail.com>
2020-03-29 22:29:35 -0300

    Alteração no design e retirada do botao maximizar

--- Cronometro/src/cronometro/Cronometro.java
+++ Cronometro/src/cronometro/Cronometro.java
@@ -5,6 +5,7 @@
  */
 package cronometro;
 
+import java.awt.Color;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.logging.Level;
@@ -39,11 +40,12 @@ public class Cronometro extends javax.swing.JFrame {
         jButton2 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        Voltar = new javax.swing.JButton();
-        Iniciar = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
         resultado = new javax.swing.JLabel();
         resultadoEsquerda = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
+        Voltar = new javax.swing.JButton();
+        Iniciar = new javax.swing.JButton();
 
         jButton2.setText("jButton2");
 
@@ -61,6 +63,22 @@ public class Cronometro extends javax.swing.JFrame {
         jScrollPane1.setViewportView(jTable1);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Cronometro");
+        setResizable(false);
+
+        jPanel1.setBackground(new java.awt.Color(0, 0, 0));
+
+        resultado.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+        resultado.setForeground(new java.awt.Color(255, 255, 255));
+        resultado.setText("00");
+
+        resultadoEsquerda.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+        resultadoEsquerda.setForeground(new java.awt.Color(255, 255, 255));
+        resultadoEsquerda.setText("00");
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText(":");
 
         Voltar.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         Voltar.setText("Zerar");
@@ -70,6 +88,7 @@ public class Cronometro extends javax.swing.JFrame {
             }
         });
 
+        Iniciar.setBackground(new java.awt.Color(102, 255, 0));
         Iniciar.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         Iniciar.setText("Iniciar");
         Iniciar.addActionListener(new java.awt.event.ActionListener() {
@@ -78,52 +97,53 @@ public class Cronometro extends javax.swing.JFrame {
             }
         });
 
-        resultado.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        resultado.setText("00");
-
-        resultadoEsquerda.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        resultadoEsquerda.setText("00");
-
-        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        jLabel2.setText(":");
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(Voltar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(Iniciar)
+                .addContainerGap())
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(96, 96, 96)
+                .addComponent(resultadoEsquerda)
+                .addGap(1, 1, 1)
+                .addComponent(jLabel2)
+                .addGap(1, 1, 1)
+                .addComponent(resultado)
+                .addContainerGap(103, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(80, 80, 80)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(resultado)
+                    .addComponent(resultadoEsquerda)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 85, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(Voltar)
+                    .addComponent(Iniciar))
+                .addContainerGap())
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(Voltar)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(Iniciar))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(116, 116, 116)
-                        .addComponent(resultadoEsquerda)
-                        .addGap(1, 1, 1)
-                        .addComponent(jLabel2)
-                        .addGap(1, 1, 1)
-                        .addComponent(resultado)
-                        .addGap(0, 113, Short.MAX_VALUE)))
-                .addContainerGap())
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(56, 56, 56)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(resultado)
-                    .addComponent(resultadoEsquerda)
-                    .addComponent(jLabel2))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 91, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(Voltar)
-                    .addComponent(Iniciar))
-                .addGap(54, 54, 54))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
     int QuanridadeDeClicks = 0;
     boolean pediuParada = false;
@@ -132,7 +152,9 @@ public class Cronometro extends javax.swing.JFrame {
     int minutos = 0;
 
     private void IniciarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_IniciarActionPerformed
-    	Thread cronometro = new Thread(new Runnable() {
+    	
+        
+        Thread cronometro = new Thread(new Runnable() {
             public void run(){
                 //Parte criada para o quando o botao "Zerar" for acionado
                 if(ClickZerar == 1){
@@ -187,13 +209,14 @@ public class Cronometro extends javax.swing.JFrame {
  
         //Variavel
         if(QuanridadeDeClicks == 0){
-        	
+        	Iniciar.setBackground(Color.red);
         	cronometro.start();
 	        
 	        Iniciar.setText("Parar");
                 QuanridadeDeClicks = 1;
 	        
-        } else if (QuanridadeDeClicks == 1) {
+        } else if (QuanridadeDeClicks == 1){
+                Iniciar.setBackground(Color.green);
         	QuanridadeDeClicks = 0;
         	pediuParada = true;
         	Iniciar.setText("Retomar");
@@ -264,6 +287,7 @@ public class Cronometro extends javax.swing.JFrame {
     private javax.swing.JButton Voltar;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
     private javax.swing.JLabel resultado;
